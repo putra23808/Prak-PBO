783868bf0c421f227f7640e410bcbf4c203a6f9b
putra23808 <mochammadfadhilap@gmail.com>
2024-10-17 18:52:18 +0700

    Add files via upload

---
+++ Tugas07_4523210063_Mochammad Fadhila Putra/Luar.java
@@ -0,0 +1,19 @@
+public class Luar {
+    private String outerVariable = "Variabel luar";
+
+    class Dalam {
+        String innerVariable = "Variabel dalam";
+
+        public void bicara() {
+            System.out.println(innerVariable);
+            System.out.println(outerVariable);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Luar l = new Luar();
+        Luar.Dalam d = l.new Dalam();
+
+        d.bicara();
+    }
+}
